8074a01d0759b38e3e5b8a6abf460b8c498c4d8e
poream3387 <poream3387@gmail.com>
2020-02-04 17:20:48 +0900

    Until 59

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,29 @@
-console.log('Client side javascript file is loaded!!')
+// This is a client side code since it is linked by <script> tag to one of the HTML files
+// So server side cannot be used here!
+
+console.log('Client side javascript file is loaded')
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // This prevents refreshing the page after submitting
+
+    messageOne.textContent = "Loading...";
+    messageTwo.textContent = "";
+
+    const location = search.value;
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        })
+    });
+})
