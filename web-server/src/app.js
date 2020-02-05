bad2d300a0ea617e77676b5997b3055262800b8f
poream3387 <poream3387@gmail.com>
2020-02-05 23:59:12 +0900

    web-server done for heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,6 +7,8 @@ const forecast = require('./utils/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000; // For Heroku exists, the first port else return 3000(local) port number
+
 
 // Define paths Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -84,6 +86,6 @@ app.get('*', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000!');
+app.listen(port, () => {
+    console.log('Server is up on port' + port);
 });
