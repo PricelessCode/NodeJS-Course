a63bfe973840daa0209bf2b9e0c1785150dc2f9a
poream3387 <poream3387@gmail.com>
2020-02-17 23:41:16 +0900

    Upto 88

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain ')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positibe number');
+            }
+        }
+    }
+});
+
+// const me = new User({
+//     name: '    Seungho',
+//     email: 'POREAM3387@gmail.com',
+//     password: 're34dsfsdf'
+// });
+
+// me.save().then(() => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const task = new Task({
+    description: 'Learn something',
+    completed: false
+});
+
+task.save().then(() => {
+    console.log(task);
+}).catch((error) => {
+    console.log(error);
+});
